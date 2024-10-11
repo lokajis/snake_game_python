03346108e31b53b7aa5755b9e0bd04494c4fed34
lokajis <alexandros.economide@gmail.com>
2024-10-11 20:40:48 +0200

    implimented the bordear of the game, the rool that you cant touch the snake body with the head and not allowing the head to move in the opposit direction of wich is currently moving

--- snake_game1.py
+++ snake_game1.py
@@ -1,6 +1,6 @@
 import turtle
 import time
-
+import random
 delay = 0.1
 # SETUP THE SCREEN
 
@@ -19,16 +19,31 @@ head.color("green")
 head.penup()
 head.goto(0,0)
 #it will be still at the midle when it starts
-head.direction = "stop"
+head.direction = "stop"  
+#Snake food
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
 #Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down" 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 
 def move():
@@ -54,6 +69,45 @@ wn.onkeypress(go_right, "Right")
 #main game loop
 while True:
     wn.update()
+        #check for collision with the border 
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() <-290:
+        time.sleep(1)  
+        head.goto(0,0)
+        head.direction = "stop"
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        #body colision reset
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()            
+
+
+    if head.distance(food) < 20:
+        #moove the food to a random spot  
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("black")
+        new_segment.penup()
+        segments.append(new_segment)
+#MOve the segments end firstn in reverse order
+    for i in range(len(segments)-1,0,-1):
+        segments[i].goto(segments[i-1].xcor() , segments[i-1].ycor())
+#move segment 0 to the hrad
+    if len(segments) > 0 :
+        segments[0].goto(head.xcor(), head.ycor())
+
+
     move()
     time.sleep(delay)
 
